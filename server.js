8413c90f506917c6c34f307f42234c3f6dec4954
fatima <fatima.jacoboruiz@gmail.com>
2016-11-14 18:44:55 -0500

    board paint

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app =  express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+//configurar archivos públicos-estáticos(css-js-img)
+
+app.use('/static',express.static(__dirname + '/static'));
+
+//configurar pug - setear variables views express
+app.set('views',__dirname + '/views');
+app.set('view engine', 'pug')
+
+//socket.io escucha los metodos implementados desde el lado del cliente y del server
+var contador = 0;
+io.on('connection', function(socket){
+	contador++;
+	io.sockets.emit('welcome', contador);
+//socket en escucha - var movimientos (que cliente pasa al server y el server se encarga de emitir a todos los clientes)
+	socket.on('draw' ,function(_movimientos){ 
+		io.sockets.emit('update', _movimientos);
+	})
+})
+
+
+app.get('/', function(req,res){
+	res.render('home',{message: 'Bienvenido a nuestro proyecto de prueba'});
+});
+
+//para iniciar el servidor
+http.listen(3000,function(){
+	console.log('El server está escuchando el puerto :3000');
+});
